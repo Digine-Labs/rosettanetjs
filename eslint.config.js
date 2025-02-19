b56fb4e07cb64851c78e90ca51f753816399fb6d
Bora <bora_atik98@hotmail.com>
2025-02-19 14:23:46 +0300

    feat: added libraries

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: process.cwd(),
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+    ignores: ['tsup.config.ts', 'eslint.config.js', 'dist', 'node_modules'],
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+    ignores: ['tsup.config.ts', 'eslint.config.js', 'dist', 'node_modules'],
+  },
+];
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// A simple function to add two numbers
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(add(2, 3));
