bbf3633e89d4ec53b7802352b8bd6557856b6bc4
Bora Atik <bora_atik98@hotmail.com>
2025-02-20 12:56:00 +0300

    chore: format

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Trial } from "./types";
+import { Trial } from './types';
 
 // A simple function to add two numbers
 export const add = (a: number, b: number): number => {
@@ -7,11 +7,11 @@ export const add = (a: number, b: number): number => {
 
 export const getTrial = (): Trial => {
   return {
-    id: "1",
-    name: "Trial 1",
-    description: "This is a trial",
-    startDate: "2021-01-01",
-    endDate: "2021-01-31",
-    status: "active",
+    id: '1',
+    name: 'Trial 1',
+    description: 'This is a trial',
+    startDate: '2021-01-01',
+    endDate: '2021-01-31',
+    status: 'active',
   };
-}
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,8 @@
 export type Trial = {
-    id: string;
-    name: string;
-    description: string;
-    startDate: string;
-    endDate: string;
-    status: string;
+  id: string;
+  name: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  status: string;
 };
